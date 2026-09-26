de1281e38c64037f4dacb30b92ea3fec455a18ef
bourdillion <isaacakhigbe22@gmail.com>
2026-09-26 14:39:41 +0100

    Add test that starts a local HTTP server

--- app/src/test/java/com/isaacakhigbe/rotwatch/LinkCheckerTest.java
+++ app/src/test/java/com/isaacakhigbe/rotwatch/LinkCheckerTest.java
@@ -2,16 +2,16 @@ package com.isaacakhigbe.rotwatch;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-import com.sun.net.httpserver.HttpsServer;
+import com.sun.net.httpserver.HttpServer;
 import java.net.InetSocketAddress;
 
 class LinkCheckerTest {
     @Test
     void serverStartsOnAFreePort() throws Exception {
-        HttpsServer server = HttpsServer.create(new InetSocketAddress(0), 0);
+        HttpServer server = HttpServer.create(new InetSocketAddress(0), 0);
         server.start();
         int port = server.getAddress().getPort();
         server.stop(0);
-        assertTrue(port >= 0);
+        assertTrue(port > 0);
     }
 }
